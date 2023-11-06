2156e699f715be447ed0c39e904e16ac6e8b04dd
Manuel Soria <ms.manuelsoria@gmail.com>
2023-11-06 01:26:46 -0300

    chat

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from langchain.schema import HumanMessage, AIMessage
 import streamlit as st
 from langsmith import Client
 
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate
+from langchain.schema import SystemMessage
+from langchain.memory import ConversationBufferMemory
+
 from utils import StreamHandler
 client = Client()
 
@@ -17,3 +21,45 @@ button_css =""".stButton>button {
 }"""
 st.markdown(f'<style>{button_css}</style>', unsafe_allow_html=True)
 
+template = """You are an excellent assistant in helping students learn about Google Apps Script.
+You will assist students in any concern. They are begginers.
+"""
+
+prompt_template = ChatPromptTemplate(messages = [SystemMessage(content=template), MessagesPlaceholder(variable_name="chat_history"), HumanMessagePromptTemplate.from_template("{input}")])
+
+from langchain.chains import LLMChain
+
+def send_feedback(run_id, score):
+    client.create_feedback(run_id, "user_score", score=score)
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [AIMessage(content="Welcome! This short course with help you started with LangChain, and will cover LLMs, prompts, output parsers, and LLMChains.Before doing this, you should have a Python environment set up. Do you have that done?")]
+
+for msg in st.session_state["messages"]:
+    if isinstance(msg, HumanMessage):
+        st.chat_message("user").write(msg.content)
+    else:
+        st.chat_message("assistant").write(msg.content)
+
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
+
+    with st.chat_message("assistant"):
+        stream_handler = StreamHandler(st.empty())
+        model = ChatOpenAI(streaming=True, callbacks=[stream_handler], model="gpt-4")
+        chain = LLMChain(prompt=prompt_template, llm=model)
+
+        response = chain({"input":prompt, "chat_history":st.session_state.messages[-20:]}, include_run_info=True)
+        st.session_state.messages.append(HumanMessage(content=prompt))
+        st.session_state.messages.append(AIMessage(content=response[chain.output_key]))
+        run_id = response["__run"].run_id
+
+        col_blank, col_text, col1, col2 = st.columns([10, 2,1,1])
+        with col_text:
+            st.text("Feedback:")
+
+        with col1:
+            st.button("👍", on_click=send_feedback, args=(run_id, 1))
+
+        with col2:
+            st.button("👎", on_click=send_feedback, args=(run_id, 0))
